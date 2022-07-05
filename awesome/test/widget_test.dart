d6801013c69fcf5aaf30cdb6d1f9a63db93b3a55
chirag kumar <chiragroy3030@gmail.com>
2022-07-05 11:57:27 +0530

    chapter1 completed

--- awesome/test/widget_test.dart
+++ awesome/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:awesome/main.dart';
+// import 'package:awesome/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
