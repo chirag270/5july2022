d33f5aef1b3cf9f3f71e8db8247893c9963d3fc6
chirag kumar <chiragroy3030@gmail.com>
2022-07-05 13:02:26 +0530

    completed chapter2

--- awesome/lib/main.dart
+++ awesome/lib/main.dart
@@ -16,18 +16,49 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("HomePage NavBaar")),
-      body: Container(
-      child: Center(child: Text("hii flutter hello"))),
-        // child: Text("hii flutter"),)
+      appBar: AppBar(title:const Text("SanChirag"),
+      backgroundColor: Colors.black87),
+      body: Center(
+        
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          
+          width: 300,
+          height: 250,
+          decoration: BoxDecoration(
+            // shape: BoxShape.rectangle,
+            
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(color: Colors.black,
+              blurRadius: 3,
+              spreadRadius: 2,
+              offset: Offset(5.0, 5.0)),
+              
+            ],
+            color: Colors.red, 
+            gradient: LinearGradient(colors: [Colors.yellow, Colors.black])
+          ),
+
+
+
+
+        child: const Text("I am Chirag ",
+        textAlign: TextAlign.center ,
+        style: TextStyle(
+          color: Colors.black87,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+
+        ),),
+      
+
+        ))
+
     );
     
     
-    
-    //  Container(
-    //   color: Colors.purple,
-    //   child: const Text("hii chirag hello"),
-    // );
     
   }
 }
